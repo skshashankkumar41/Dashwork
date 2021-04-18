64dee9b03d8b53574b5986e107cf21e26071fe81
Shashank Srivastava <skshashankkumar41@gmail.com>
2021-04-19 03:51:50 +0530

    minor css change

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -6,6 +6,7 @@ export const HeaderContainer = styled.div`
   background-color: #a3bcb6;
   padding: 0;
   margin: 0;
+  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.12), 0 1px 4px rgba(0, 0, 0, 0.24);
 `;
 
 export const HeaderTitle = styled.div`
