506f7c9971b257e2549b79b042b0e159f05fee8b
Shashank Srivastava <skshashankkumar41@gmail.com>
2021-04-20 02:21:51 +0530

    added add intent button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import * as s from "./Header.styles";
 
 const Header = (props) => {
+  let button;
+  if (props.headerName === "Intent") {
+    button = (
+      <s.HeaderButton
+        onClick={() => {
+          console.log("BUTTON CLICKED");
+        }}
+      >
+        Add Intent
+      </s.HeaderButton>
+    );
+  } else {
+    button = null;
+  }
+
   return (
     <s.HeaderContainer>
       <s.HeaderTitle>{props.headerName}</s.HeaderTitle>
+      {button}
     </s.HeaderContainer>
   );
 };
--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -7,6 +7,7 @@ export const HeaderContainer = styled.div`
   padding: 0;
   margin: 0;
   box-shadow: 0 1px 6px rgba(0, 0, 0, 0.12), 0 1px 4px rgba(0, 0, 0, 0.24);
+  display: flex;
 `;
 
 export const HeaderTitle = styled.div`
@@ -16,6 +17,22 @@ export const HeaderTitle = styled.div`
   font-weight: bold;
   padding-left: 15px;
   padding-top: 8px;
+`;
+
+export const HeaderButton = styled.button`
+  background-color: #9fccc2;
+  border-radius: 12px;
+  position: absolute;
+  text-align: center;
+  right: 4px;
+  top: 6px;
+  height: 30px;
+  width: 125px;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  color: white;
+  font-size: 17px;
 `;
 
 //#dce1e3;
